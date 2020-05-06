e01bc59765b034d2e5e20e907e5c6655a1b0248d
jojoccp <jonatas.ccp@gmail.com>
2020-05-06 01:20:18 -0300

    Implementaçao da tela de login com Spring Security e ThymeLeaf ... redirecionamento após login: ok

---
+++ src/test/java/io/github/jojoccp/customlogin/CustomLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.jojoccp.customlogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
